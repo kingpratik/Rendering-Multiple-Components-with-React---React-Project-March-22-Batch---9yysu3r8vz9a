ca4d4f0c66aaf2d87c70c70cf81ccbfa6e1997d2
PRATIK SUDHAKAR WAKODE <109046380+kingpratik@users.noreply.github.com>
2022-11-01 13:17:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test="project-name">Pratik Sudhakar Wakode</h1>
-      <p data-ns-test="project-description"></p>
+      <h1 data-ns-test="project-name">Find out The Quantity for Making Molar Solution </h1>
+      <p data-ns-test="project-description">In College lab or in chemical indusrty they need solutions/solvent for perform their experiments.that time we proide a accuracy for that soluions preparation</p>
     </div>
   )
 }
